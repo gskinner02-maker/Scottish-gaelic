7fd2dfe1110b44b418e8d1035938e838416cc487
gskinner02-maker <gskinner02@gmail.com>
2026-03-08 11:46:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,20 @@
 // VERSION
 // -----------------------------
 
-const APP_VERSION = "2.0.0";
+const APP_VERSION = "1.1.4";
 window.addEventListener("DOMContentLoaded", () => {
-    console.log("appVersion element is:", document.getElementById("appVersion"));
     const v = document.getElementById("appVersion");
     if (v) v.textContent = APP_VERSION;
 });
 
-console.log("Loaded script.js v1.1");
+//const APP_VERSION = "2.0.0";
+//window.addEventListener("DOMContentLoaded", () => {
+//    console.log("appVersion element is:", document.getElementById("appVersion"));
+//    const v = document.getElementById("appVersion");
+//    if (v) v.textContent = APP_VERSION;
+//});
+
+//console.log("Loaded script.js v1.1");
 
 // -----------------------------
 // FUTURE VERB LISTS
@@ -1319,6 +1325,7 @@ function saveFutureIrregular() {
     backFromEditor();
 
 }
+
 
 
 
